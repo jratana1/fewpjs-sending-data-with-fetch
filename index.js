cd55a317bf6531473b3a2158b084c5773e205115
John Ratana <jratana1@gmail.com>
2021-01-29 12:30:35 -0500

    commit tree 02b6b3c4ac1d5fb6a48e8d948b8a85daf4b9fdc1

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+function  submitData(name, email){ 
+    let formData = {
+        name: `${name}`,
+        email: `${email}`
+      };
+    
+    let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+      let body = document.getElementsByTagName("body")
+      body[0].append(object.id)
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+      let body = document.getElementsByTagName("body")
+      body[0].append(error.message)
+    });
+}
